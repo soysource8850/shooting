e5770f8e1a99d4b836d5a92ce07264745b57c1b8
soysource8850 <lucid.sky.snl@gmail.com>
2019-07-02 19:05:27 +0900

    feat: add class Rectangle

--- engine.js
+++ engine.js
@@ -1,1 +1,17 @@
 'use strict';
+
+class Rectangle {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  hitTest(other) {
+    const horizontal = (other.x < this.x + this.width) && (this.x < other.x + other.width);
+    const vertical = (other.y < this.y + this.height) && (this.y < other.y < other.height);
+
+    return (horizontal && vertical);
+  }
+}
