3e363b50e2021858fe337415b908c2ffc646411d
soysource8850 <lucid.sky.snl@gmail.com>
2019-07-03 18:42:44 +0900

    feat: add class Input (retain key of previous and current)

--- engine.js
+++ engine.js
@@ -157,3 +157,40 @@ class SpriteActor extends Actor {
     return (horizontal || vertical);
   }
 }
+
+class Input {
+  constructor(keyMap, prevKeyMap) {
+    this.keyMap = keyMap;
+    this.prevKeyMap = prevKeyMap;
+  }
+
+  _getKeyFromMap(keyName, map) {
+    if (map.has(keyName)) {
+      return map.get(keyName);
+    } else {
+      return false;
+    }
+  }
+
+  _getPrevKey(keyName) {
+    return this._getKeyFromMap(keyName, this.prevKeyMap);
+  }
+
+  getKey(keyName) {
+    return this._getKeyFromMap(keyName, this.keyMap);
+  }
+
+  getKeyDown(keyName) {
+    const prevDown = this._getPrevKey(keyName);
+    const currentDown = this.getKey(keyName);
+
+    return (!prevDown && currentDown);
+  }
+
+  getKeyUp(keyName) {
+    const prevDown = this._getPrevKey(keyName);
+    const currentDown = this.getKey(keyName);
+
+    return (prevDown && !currentDown);
+  }
+}
