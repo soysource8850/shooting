8847dcad8693fa7d023d591c7e5686da3885b9b3
soysource8850 <lucid.sky.snl@gmail.com>
2019-07-04 15:50:48 +0900

    feat: add scenes

--- danmaku.js
+++ danmaku.js
@@ -38,3 +38,27 @@ class Fighter extends SpriteActor {
     }
   }
 }
+
+class DanmakuStgMainScene extends Scene {
+  constructor(renderingTarget) {
+    super('メイン', 'black', renderingTarget);
+    const fighter = new Fighter(150, 300);
+    this.add(fighter);
+  }
+}
+
+class DanmakuStgTitleScene extends Scene {
+  constructor(renderingTarget) {
+    super('タイトル', 'black', renderingTarget);
+    const title = new Title(100, 200);
+    this.add(title);
+  }
+
+  update(gameInfo, input) {
+    super.update(gameInfo, input);
+    if (input.getKeyDown(' ')) {
+      const mainScene = new DanmakuStgMainScene(this.renderingTarget);
+      this.changeScene(mainScene);
+    }
+  }
+}
