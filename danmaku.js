ad8663d8353dfc8eaec34511357d31cbe95b2a40
soysource8850 <lucid.sky.snl@gmail.com>
2019-07-05 16:46:51 +0900

    fix: rename class Title to TextLabel and add argument "text".

--- danmaku.js
+++ danmaku.js
@@ -1,16 +1,18 @@
 'use strict';
 
-class Title extends Actor {
-  constructor(x, y) {
+class TextLabel extends Actor {
+  constructor(x, y, text) {
     const hitArea = new Rectangle(0, 0, 0, 0);
     super(x, y, hitArea);
+
+    this.text = text;
   }
 
   render(target) {
     const context = target.getContext('2d');
     context.font = '25px sans-serif';
     context.fillStyle = 'white';
-    context.fillText('弾幕STG', this.x, this.y);
+    context.fillText(this.text, this.x, this.y);
   }
 }
 
