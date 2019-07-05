4dbe5895665bb0763cd3fcfdc04b3130feb848c4
soysource8850 <lucid.sky.snl@gmail.com>
2019-07-05 11:36:39 +0900

    feat: add Enemy and EnemyHpBar on the DanmakuStgMainScene

--- danmaku.js
+++ danmaku.js
@@ -145,7 +145,11 @@ class DanmakuStgMainScene extends Scene {
   constructor(renderingTarget) {
     super('メイン', 'black', renderingTarget);
     const fighter = new Fighter(150, 300);
+    const enemy = new Enemy(150, 100);
+    const hpBar = new EnemyHpBar(50, 20, enemy);
     this.add(fighter);
+    this.add(enemy);
+    this.add(hpBar);
   }
 }
 
